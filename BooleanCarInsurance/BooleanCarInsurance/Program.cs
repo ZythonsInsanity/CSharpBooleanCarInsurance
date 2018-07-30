68f05f46cf1fbb6223fada7e189cb605b412dd88
ZythonsInsanity <37195840+ZythonsInsanity@users.noreply.github.com>
2018-07-30 16:25:41 -0700

    Uploading assignment to repository.

---
+++ BooleanCarInsurance/BooleanCarInsurance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("What is your age?");
+        int yourAge = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Have you ever had a DUI? Please answer True or False.");
+        bool hadDUI = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("How many speeding tickets do you have?");
+        int speedTix = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Are you qualified for our insurance?");
+        bool amiQualifiedage = (yourAge <= 15);
+        bool tixQualified = (speedTix >= 3);
+        Console.WriteLine("Based on what you've entered, your qualifications are {0}", amiQualifiedage==tixQualified!=hadDUI);
+        Console.ReadLine();
+        }
+    }
+
